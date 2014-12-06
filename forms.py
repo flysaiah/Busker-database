2a065550bac15c08e9683146f1fe3235130970a1
riknos314 <riknos314@gmail.com>
2014-12-06 16:06:49 -0600

    more template upgrades

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import Required
 class SignupForm(Form):
 	new_email_username = StringField('new_email_username', validators=[Required()])
 	new_password = PasswordField('new_password', validators=[Required()])
-	performer_option = BooleanField('performer_option')
+	performer_option = BooleanField('performer_option', default=False)
 
 class LoginForm(Form):
 	email_username = StringField('email_username', validators=[Required()])
