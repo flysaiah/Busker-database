4fd8155361cb6cd937c6950cbe10f64df0737ddd
riknos314 <riknos314@gmail.com>
2014-12-06 16:32:56 -0600

    TEMPLATES BRO

--- mainfile.py
+++ mainfile.py
@@ -9,7 +9,7 @@ app.config.from_object('config')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
-	if form.validate_on_submit():
+	#if form.validate_on_submit():
 		#do something
 	return render_template('login.html', form=form)
 
@@ -17,6 +17,6 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 	form = SignupForm()
-	if form.validate_on_submit():
+	#if form.validate_on_submit():
 		#do something
 	return render_template('signup.html', form=form)
