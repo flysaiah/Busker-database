7d4ca8988a01ffa456d82f54cc4c479d1c7a7bb9
flysaiah <mayeis01@8258.dynamic.luther.edu>
2014-12-06 17:03:06 -0600

    More work on tables.py

--- tables.py
+++ tables.py
@@ -9,11 +9,12 @@ class Performer(db.Model):
 	performer_email = db.Column(db.Text, primary_key=True)
 	name = db.Column(db.Text)
 	performer_password = db.Column(db.Text)
+	performances = db.relationship('Concert', secondary=performances, backref='performers')
 
 	def __init__(self, name, performer_email, performer_password):
 		self.performer_email = performer_email
 		self.name = name
-		self.performer_password = performer_password)
+		self.performer_password = performer_password
 	
 	def __repr__(self):
 		return '<Performer {0}>'.format(self.performer_email)	
@@ -21,6 +22,7 @@ class Performer(db.Model):
 class User(db.Model):
 	user_email = db.Column(db.Text, primary_key=True)
 	user_password = db.Column(db.Text)
+	favorites = db.relationship('Performer', secondary=favorites, backref='followers')
 
 	def __init__(self, user_email, user_password):
 		self.user_email = user_email
@@ -33,6 +35,7 @@ class Concert(db.Model):
 	time = db.Column(db.DateTime)
 	place = db.Column(db.Text)
 	owner = db.Column(db.Text)
+	attendees = db.relationship('User', secondary=attendings, backref='concerts')
 
 	def __init__(self, time, place, owner):
 		self.time = time
@@ -41,5 +44,6 @@ class Concert(db.Model):
 	def __repr__(self):
 		return '<Concert at {0}, {1}>'.format(self.place, self.time)
 
-performance = db.Table('performance', db.Column('email_of_performer', db.Text, db.ForeignKey('performer.performer_email')), db.Column('concert_id', db.Integer, db.ForeignKey('concert.generated_id')))
-
+performances = db.Table('performances', db.Column('email_of_performer', db.Text, db.ForeignKey('performer.performer_email')), db.Column('concert_id', db.Integer, db.ForeignKey('concert.generated_id')))
+favorites = db.Table('favorites', db.Column('p_email', db.Text, db.ForeignKey('performer.performer_email')), db.Column('u_email', db.Text, db.ForeignKey('user.user_email')))
+attendings = db.Table('attendings', db.Column('email_of_user', db.Text, db.ForeignKey('user.user_email')), db.Column('id_of_concert', db.Integer, db.ForeignKey('concert.generated_id')))
